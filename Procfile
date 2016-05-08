b913e44496d1042caa6afc0a688a0649763d637c
Oliver Martell <oliver.martell@gmail.com>
2016-05-08 19:57:38 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/uberjar/confhub-standalone.jar
---
+++ src/confhub/endpoint/example.clj
@@ -0,0 +1,8 @@
+(ns confhub.endpoint.example
+  (:require [compojure.core :refer :all]
+            [clojure.java.io :as io]))
+
+(defn example-endpoint [config]
+  (context "/example" []
+    (GET "/" []
+      (io/resource "confhub/endpoint/example/example.html"))))
